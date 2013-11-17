6e4bb975ecfa9c0bcfd08ee7f7d0d0cbc3fc6b85
Bryce Hill <bahill@godaddy.com>
2013-11-17 13:19:56 -0700

    Add Default non specific config object

---
+++ config.js
@@ -0,0 +1,25 @@
+
+var config = {
+    mailchimp: {
+        key: '',
+        list: {
+            id: ''
+        }
+    }
+
+    // Maybe eventually customize these
+    // development: {
+    //     url: '',
+    // },
+
+    // production: {
+    //     url: '',
+    //     server: {
+    //         host: '127.0.0.1',
+    //         port: '2368'
+    //     }
+    // }
+};
+
+// Export config
+module.exports = config;
