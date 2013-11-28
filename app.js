6bc1cb4b08efc3a70b4740c4614eda026d7c0c7b
Bryce Hill <bryceahill@gmail.com>
2013-11-27 21:04:07 -0700

    Add custom favicon

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.configure(function() {
 
   app.use(express.static(__dirname + '/public'));
   app.use(express.logger('dev'));
-  app.use(express.favicon());
+  app.use(express.favicon(__dirname + '/public/images/favicon.ico'));
 
   app.use(app.router);
 
