81d208ed363fba671003ea894897aeb9964746f1
Bryce Hill <bryceahill@gmail.com>
2013-12-16 08:23:34 -0700

    Remove api key

--- config.js
+++ config.js
@@ -2,15 +2,14 @@
 var config = {
 
   mailchimp: {
-    key: '783765d1c7a5958f39f247a966d9e795-us3',
+    key: '',
     list: {
-      id: 'd14ab9e32e'
+      id: ''
     }
   },
   instagram: {
     userId: '',
-    accessToken: '',
-    client: {
+    accessToken: ''
       id: '',
       secret: ''
     }
